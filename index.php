263131304179bf2fc55ae5fa6c3013f7b5089ec6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-21 19:35:17 -0600

    update

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Secret Santa</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    Testing
+    <h1>Welcome to Secret Santa</h1>
+    <div>
+        <a href="create_room.php">Create a Room</a> | <a href="join_room.php">Join a Room</a>
+    </div>
 </body>
 </html>
